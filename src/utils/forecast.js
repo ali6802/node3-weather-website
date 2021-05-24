e65543b554032007988cb94ad3ff65e4129b6b8d
ali6802 <ali6802.dev@gmail.com>
2021-05-24 10:41:48 -0300

    Add more sentences

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,7 @@ const forecast = (latitude, longitude, callback) => {
         } else if (response.body.error) {
             callback('Unable to find location', undefined)
         } else {
-            callback(undefined, response.body.current.weather_descriptions[0] + ". It is currently " + response.body.current.temperature + " degress out.")
+            callback(undefined, response.body.current.weather_descriptions[0] + ". It is currently " + response.body.current.temperature + " F out."+"It feels like "+response.body.current.feelslike+" F . Humidity is "+response.body.current.humidity+"%, The cloudcover is "+response.body.current.cloudcover+"%")
         }
     })
 }
